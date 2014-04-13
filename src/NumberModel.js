341e6581c5bdea2b7a59b875c18995f6ed7d988d
zhao-li- <s1216186168@gmail.com>
2014-04-13 15:03:28 +0800

    make the random number can start with 0

--- src/NumberModel.js
+++ src/NumberModel.js
@@ -1,12 +1,7 @@
 function random_a_number() {
     var numbers = [];
-    var thousand = parseInt(Math.random() * 9 + 1);
-    numbers.push(thousand);
-    while (numbers.length < 4) {
+    for(var i=0;i<4;i++){
         var number = parseInt(Math.random() * 10);
-        if (!_.contains(numbers, number)) {
-            numbers.push(number);
-        }
     }
     return numbers[0] * 1000 + numbers[1] * 100 + numbers[2] * 10 + numbers[3];
 }
